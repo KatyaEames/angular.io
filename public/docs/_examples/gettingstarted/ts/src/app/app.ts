763f34548b4df38471b50d8e2c00a856b398b03a
Ward Bell <wardbell@hotmail.com>
2015-09-23 21:17:02 -0700

    (docs) update gettingstarted to our current recommendation (still needs to be vetted)

--- public/docs/_examples/gettingstarted/ts/src/app/app.ts
+++ public/docs/_examples/gettingstarted/ts/src/app/app.ts
@@ -1,9 +1,19 @@
+// #docregion
+// #docregion import
 import {Component, View, bootstrap} from 'angular2/angular2';
+// #enddocregion import
+// #docregion class-w-annotations
 @Component({
   selector: 'my-app'
 })
 @View({
   template: '<h1 id="output">My First Angular 2 App</h1>'
 })
+// #docregion class
 class AppComponent { }
+// #enddocregion class
+// #enddocregion class-w-annotations
+
+// #docregion bootstrap
 bootstrap(AppComponent);
+// #enddocregion bootstrap
